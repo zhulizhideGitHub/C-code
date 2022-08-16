ca135f8d327d9198e9092264f23c2d9d7e898118
zhulizhideGitHub <18506571953@163.com>
2022-08-16 16:22:33 +0800

    array

---
+++ 数组.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr[10] = {1,2,3,4,5,6,7,8,9,10}; 
+	//int arr[5] = {11,12,13,14,15};
+	int i = 0;
+	//int o = 10;
+	scanf("%d%d",&arr[i],&arr[o]);
+	while(i<10)
+	{
+		printf("%d",arr[i]);
+		i++;
+	
+	
+	}
+	/*else
+		printf("%d",arr[o]);
+	    o++;*/
+	
+
+
+
+
+
+	return 0;
+}
