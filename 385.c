6dcfb35c5a0b5bd3710b52a5a1a771d2384090cc
zhulizhideGitHub <18506571953@163.com>
2022-08-22 23:53:33 +0800

    operator

---
+++ 385.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+
+	char ab1 = 0;
+	int ab = 0;
+	int x = 0;
+	
+	scanf("ab1 % ab ",x);
+	printf("%d ",x);
+
+
+
+	return 0;
+
+}
