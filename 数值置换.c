e9b20f19a8e88087b8a7839db457b40857faa62c
zhulizhideGitHub <18506571953@163.com>
2022-08-23 23:01:39 +0800

    replace

---
+++ 数值置换.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d %d %d",&a,&b,&c);
+	if(a<b)
+	{
+		int tmp = a;
+		a = b;
+		b = tmp;
+	}
+	if(b<c)
+	{
+		int tmp = b;
+		b = c;
+		c = tmp;
+	}
+	printf("%d %d %d\n",a, b, c);
+
+
+	return 0;
+}
