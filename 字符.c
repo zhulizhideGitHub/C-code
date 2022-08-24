9a5d9f85b0c5c9ab22345be2adfc71eefba25f06
zhulizhideGitHub <18506571953@163.com>
2022-08-25 00:28:52 +0800

    arr

---
+++ 字符.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+int main()
+{
+	char zf1[] = "abc";
+	char zf2[] ={'a','b','c','\0'};
+	printf("%s\n",zf1);
+	printf("%s\n",zf2);
+	return 0;
+
+
+}
